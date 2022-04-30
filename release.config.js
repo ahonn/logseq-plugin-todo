6d228dc9238acfef1f0e66f16e63ff04a0049816
ahonn <ahonn95@outlook.com>
2022-04-30 11:04:47 +0800

    fix: fix missing logo.svg after build dist

--- release.config.js
+++ release.config.js
@@ -20,13 +20,13 @@ module.exports = {
       "@semantic-release/exec",
       {
         prepareCmd:
-          "zip -qq -r logseq-plugin-template-react-${nextRelease.version}.zip dist readme.md LICENSE package.json",
+          "zip -qq -r logseq-plugin-todo-${nextRelease.version}.zip dist readme.md LICENSE package.json logo.svg",
       },
     ],
     [
       "@semantic-release/github",
       {
-        assets: "logseq-plugin-template-react-*.zip",
+        assets: "logseq-plugin-todo-*.zip",
       },
     ],
   ],
